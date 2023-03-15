a16b86b3e79f8a7622a8dc468c5dff8800d48a4c
Carlos Alonso <calonsogeek@gmail.com>
2023-03-15 09:35:34 -0600

    mas campos: sector, publico_meta y formato_actividad

--- protected/views/semana/_form.php
+++ protected/views/semana/_form.php
@@ -5,32 +5,31 @@
 ?>
 
 <style>
-.errorSummary, .errorMessage
-{
-	color: red;	
-}
+	.errorSummary,
+	.errorMessage {
+		color: red;
+	}
 </style>
 
 <script type="text/javascript">
-	$(document).ready(function (){
-		$('#actividad').change(function (){
-			if ($(this).val() == '0') 
-			{
+	$(document).ready(function() {
+		$('#actividad').change(function() {
+			if ($(this).val() == '0') {
 				$('#Semana_otra_actividad').prop('disabled', false);
 			} else {
 				$('#Semana_otra_actividad').prop('disabled', true);
 			}
-		});		
+		});
 	});
 </script>
 
 <div class="form">
 
-	<?php $form=$this->beginWidget('CActiveForm', array(
-			'id'=>'semana-form',
-			'enableAjaxValidation'=>false,
-			'htmlOptions'=>array('enctype'=>'multipart/form-data'),
-)); ?>
+	<?php $form = $this->beginWidget('CActiveForm', array(
+		'id' => 'semana-form',
+		'enableAjaxValidation' => false,
+		'htmlOptions' => array('enctype' => 'multipart/form-data'),
+	)); ?>
 
 	<p class="note">
 		Campos con <span class="required">*</span> son requeridos.
@@ -42,133 +41,182 @@
 		<tr>
 			<td>
 				<div>
-					<?php echo $form->labelEx($model,'institucion'); ?>
-					<?php echo $form->textField($model,'institucion',array('size'=>60,'maxlength'=>255)); ?>
-					<?php echo $form->error($model,'institucion'); ?>
+					<?php echo $form->labelEx($model, 'institucion'); ?>
+					<?php echo $form->textField($model, 'institucion', array('size' => 60, 'maxlength' => 255)); ?>
+					<?php echo $form->error($model, 'institucion'); ?>
 				</div>
 			</td>
-			<td><?php $this->renderPartial('_logo',array(
-					'model'=>$model, 'form'=>$form
-			)); ?></td>
+			<td><?php $this->renderPartial('_logo', array(
+					'model' => $model, 'form' => $form
+				)); ?></td>
 		</tr>
 	</table>
 
+	<br />
 	<div>
-		<?php echo $form->labelEx($model,'estado_id'); ?>
-		<?php echo $form->dropDownList($model, 'estado_id',  CHtml::listData(Estado::model()->findAll(), 'id', 'nombre'), 
-				array('empty'=>'---Selecciona un estado---', 'id'=>'estado')); ?>
-		<?php echo $form->error($model,'estado_id'); ?>
+		<?php echo $form->labelEx($model, 'sector'); ?>
+		<?php echo $form->dropDownList(
+			$model,
+			'sector',
+			Semana::sector(),
+			array('empty' => '--- ¿A qué sector perteneces? ---', 'id' => 'sector')
+		); ?>
+		<?php echo $form->error($model, 'sector'); ?>
+	</div>
+	<br />
+
+	<br />
+	<div>
+		<?php echo $form->labelEx($model, 'publico_meta'); ?>
+		<?php echo $form->dropDownList(
+			$model,
+			'publico_meta',
+			Semana::publico_meta(),
+			array('empty' => '--- ¿A quién va dirigido? ---', 'id' => 'publico_meta')
+		); ?>
+		<?php echo $form->error($model, 'publico_meta'); ?>
+	</div>
+	<br />
+
+	<div>
+		<?php echo $form->labelEx($model, 'estado_id'); ?>
+		<?php echo $form->dropDownList(
+			$model,
+			'estado_id',
+			CHtml::listData(Estado::model()->findAll(), 'id', 'nombre'),
+			array('empty' => '---Selecciona un estado---', 'id' => 'estado')
+		); ?>
+		<?php echo $form->error($model, 'estado_id'); ?>
+	</div>
+	<br>
+
+	<br />
+	<div>
+		<?php echo $form->labelEx($model, 'formato_actividad'); ?>
+		<?php echo $form->dropDownList(
+			$model,
+			'formato_actividad',
+			Semana::formato_actividad(),
+			array('empty' => '--- ¿Cómo se llevará a cabo el evento? ---', 'id' => 'formato_actividad')
+		); ?>
+		<?php echo $form->error($model, 'formato_actividad'); ?>
+	</div>
+	<br />
+
+	<div>
+		<?php echo $form->labelEx($model, 'direccion'); ?><br>
+		<?php echo $form->textArea($model, 'direccion', array('rows' => 4, 'cols' => 50, "placeholder" => "Puede ser una dirección, punto de interés, URL de facebook, etc.")); ?>
+		<?php echo $form->error($model, 'direccion'); ?>
 	</div>
 	<br>
 	<div>
-		<?php echo $form->labelEx($model,'direccion'); ?><br>
-		<?php echo $form->textArea($model,'direccion',array('rows'=>4, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'direccion'); ?>
-	</div>
-	<br>
-	<div>
-		<?php echo $form->labelEx($model,'actividad'); ?>
-		<?php echo $form->dropDownList($model, 'actividad', Semana::actividades(), 
-				array('empty'=>'---Selecciona una actividad---', 'id'=>'actividad')); ?>
-		<?php echo $form->error($model,'actividad'); ?>
+		<?php echo $form->labelEx($model, 'actividad'); ?>
+		<?php echo $form->dropDownList(
+			$model,
+			'actividad',
+			Semana::actividades(),
+			array('empty' => '---Selecciona una actividad---', 'id' => 'actividad')
+		); ?>
+		<?php echo $form->error($model, 'actividad'); ?>
 	</div>
 	<div>
-		<?php echo $form->labelEx($model,'otra_actividad'); ?>
+		<?php echo $form->labelEx($model, 'otra_actividad'); ?>
 		<span><em>Solo si tu actividad no se encuentra en la lista</em> </span><br>
-		<?php echo $form->textField($model,'otra_actividad',array('size'=>60,'maxlength'=>255, 'disabled'=>$model->actividad == '0' ? false : true)); ?>
-		<?php echo $form->error($model,'otra_actividad'); ?>
+		<?php echo $form->textField($model, 'otra_actividad', array('size' => 60, 'maxlength' => 255, 'disabled' => $model->actividad == '0' ? false : true)); ?>
+		<?php echo $form->error($model, 'otra_actividad'); ?>
 	</div>
 	<br>
 	<div>
-		<?php echo $form->labelEx($model,'descripcion'); ?><br>
-		<?php echo $form->textArea($model,'descripcion',array('rows'=>4, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'descripcion'); ?>
+		<?php echo $form->labelEx($model, 'descripcion'); ?><br>
+		<?php echo $form->textArea($model, 'descripcion', array('rows' => 4, 'cols' => 50)); ?>
+		<?php echo $form->error($model, 'descripcion'); ?>
 	</div>
 	<br>
 	<table cellpadding="20">
 		<tr>
 			<td>
 				<div>
-					<?php echo $form->labelEx($model,'fecha_ini'); ?>
-					<?php 
+					<?php echo $form->labelEx($model, 'fecha_ini'); ?>
+					<?php
 					$this->widget(
-		'ext.jui.EJuiDateTimePicker',
-		array(
-				'model'     => $model,
-				'attribute' => 'fecha_ini',
-				//'flat'=>true,
-				//'value' => '2014-03-14 14:00:00',
-				//'language'=> 'ru',//default Yii::app()->language
-				//'mode'    => 'datetime',//'datetime' or 'time' ('datetime' default)
-				'options'   => array(
-						'dateFormat' => 'yy-mm-dd',
-						//'timeFormat' => 'hh:mm',
-						'hourMin'=>6,
-						'hourMax'=>22,
-						'minDate'=>$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio),
-						'maxDate'=>$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino),
-				),
-				'htmlOptions'=>array(
-					//'disabled'=>true
-				),
-		)
-);
-?>
-					<?php echo $form->error($model,'fecha_ini'); ?>
+						'ext.jui.EJuiDateTimePicker',
+						array(
+							'model'     => $model,
+							'attribute' => 'fecha_ini',
+							//'flat'=>true,
+							//'value' => '2014-03-14 14:00:00',
+							//'language'=> 'ru',//default Yii::app()->language
+							//'mode'    => 'datetime',//'datetime' or 'time' ('datetime' default)
+							'options'   => array(
+								'dateFormat' => 'yy-mm-dd',
+								//'timeFormat' => 'hh:mm',
+								'hourMin' => 6,
+								'hourMax' => 22,
+								'minDate' => $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio),
+								'maxDate' => $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino),
+							),
+							'htmlOptions' => array(
+								//'disabled'=>true
+							),
+						)
+					);
+					?>
+					<?php echo $form->error($model, 'fecha_ini'); ?>
 				</div>
 			</td>
 			<td>
 				<div>
-					<?php echo $form->labelEx($model,'fecha_fin'); ?>
-					<?php 
+					<?php echo $form->labelEx($model, 'fecha_fin'); ?>
+					<?php
 					$this->widget(
-		'ext.jui.EJuiDateTimePicker',
-		array(
-				'model'     => $model,
-				'attribute' => 'fecha_fin',
-				//'flat'=>true,
-				//'value' => '2014-03-14 14:00:00',
-				//'language'=> 'ru',//default Yii::app()->language
-				//'mode'    => 'datetime',//'datetime' or 'time' ('datetime' default)
-				'options'   => array(
-						'dateFormat' => 'yy-mm-dd',
-						//'timeFormat' => 'hh:mm',
-						'hourMin'=>6,
-						'hourMax'=>22,
-						'minDate'=>$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio),
-						'maxDate'=>$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino),
-				),
-				'htmlOptions'=>array(
-					//'style'=>'height:20px',
-				),
-		)
-);
-?>
-					<?php echo $form->error($model,'fecha_fin'); ?>
+						'ext.jui.EJuiDateTimePicker',
+						array(
+							'model'     => $model,
+							'attribute' => 'fecha_fin',
+							//'flat'=>true,
+							//'value' => '2014-03-14 14:00:00',
+							//'language'=> 'ru',//default Yii::app()->language
+							//'mode'    => 'datetime',//'datetime' or 'time' ('datetime' default)
+							'options'   => array(
+								'dateFormat' => 'yy-mm-dd',
+								//'timeFormat' => 'hh:mm',
+								'hourMin' => 6,
+								'hourMax' => 22,
+								'minDate' => $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio),
+								'maxDate' => $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino),
+							),
+							'htmlOptions' => array(
+								//'style'=>'height:20px',
+							),
+						)
+					);
+					?>
+					<?php echo $form->error($model, 'fecha_fin'); ?>
 				</div>
 			</td>
 		</tr>
 	</table>
 
 	<div>
-		<?php echo $form->labelEx($model,'informes'); ?>
-		<span><em>, Referencia en caso de dudas o sugerencias</em> </span><br>
-		<?php echo $form->textArea($model,'informes',array('rows'=>4, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'informes'); ?>
+		<?php echo $form->labelEx($model, 'informes');
+		?>
+		<span><em>en caso de dudas o sugerencias</em> </span><br>
+		<?php echo $form->textArea($model, 'informes', array('rows' => 4, 'cols' => 50, "placeholder" => "Puede ser un nombre, teléfono, whatsapp, correo electrónico, etc")); ?>
+		<?php echo $form->error($model, 'informes'); ?>
 	</div>
 	<br>
 	<div>
-		<?php echo $form->labelEx($model,'url'); ?>
-		<span><em>Se vinculará el evento con la URL de su página web (ej. https://www.biodiversidad.gob.mx)</em> </span><br>
-		<?php echo $form->textField($model,'url',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'url'); ?>
+		<?php echo $form->labelEx($model, 'url'); ?>
+		<span><em>para más detalles</em> </span><br>
+		<?php echo $form->textField($model, 'url', array('size' => 60, 'maxlength' => 255, "placeholder" => "ej. https://www.biodiversidad.gob.mx")); ?>
+		<?php echo $form->error($model, 'url'); ?>
 	</div>
 	<br>
-	<?php $this->renderPartial('_materiales',array(
-					'model'=>$model, 'form'=>$form, 'model_materiales' => isset($model_materiales) ? $model_materiales : array(),
-			)); ?>
+	<?php $this->renderPartial('_materiales', array(
+		'model' => $model, 'form' => $form, 'model_materiales' => isset($model_materiales) ? $model_materiales : array(),
+	)); ?>
 
-	<?php echo $form->hiddenField($model,'usuarios_id', array('value' => $usuario)); ?>
+	<?php echo $form->hiddenField($model, 'usuarios_id', array('value' => $usuario)); ?>
 
 	<div align="right">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Crear evento' : 'Actualizar evento'); ?>
--- protected/views/semana/admin.php
+++ protected/views/semana/admin.php
@@ -2,9 +2,9 @@
 /* @var $this SemanaController */
 /* @var $model Semana */
 
-$this->menu=array(
-		array('label'=>'Tus eventos', 'url'=>array('index')),
-		array('label'=>'Registra un evento', 'url'=>array('create')),
+$this->menu = array(
+	array('label' => 'Tus eventos', 'url' => array('index')),
+	array('label' => 'Registra un evento', 'url' => array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -29,48 +29,72 @@ Yii::app()->clientScript->registerScript('search', "
 
 <?php $actividades = Semana::actividades(); ?>
 <?php $this->widget('zii.widgets.grid.CGridView', array(
-		'id'=>'semana-grid',
-		'dataProvider'=>$model->search(),
-		'filter'=>$model,
-		'columns'=>array(
-				array(
-						'name'=>'estado_id',
-						'value'=>'Estado::model()->findByPk($data->estado_id)->nombre',
-						'filter'=>CHtml::listData(Estado::model()->findAll(array('order'=>'nombre ASC')), 'id', 'nombre'),
-						'sortable'=>false,
-				),
-				array(
-						'type'=>'raw',
-						'name'=>'actividad',
-						'value'=>'$data->actividad == "0" ? "<b>".$data->otra_actividad."</b><br>".(strlen($data->descripcion) <= 90 ? $data->descripcion: (substr($data->descripcion,0,90))." ...") : "<b>".Semana::actividades($data->actividad)."</b><br>".(strlen($data->descripcion) <= 90 ? $data->descripcion : (substr($data->descripcion,0,90))." ...")',
-						'filter'=>Semana::actividades(),
-						//'htmlOptions'=>array('style'=>'width:40px'),
-						'sortable'=>false,
-				),
-				array(
-						'header'=>'Fecha de inicio'.CHtml::image(Yii::app()->request->baseUrl."/imagenes/aplicacion/up-down.png", 'arriba-abajo', array('width'=>20)),
-						'name'=>'fecha_ini',
-						'filter'=>"<input type='date' name='Semana[fecha_ini]' min='".$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio)."' max='".$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino)."' value='".$model->fecha_ini."'>",
-						'value'=>'SemanaController::fechaEvento($data->fecha_ini)',
-						'htmlOptions'=>array('style'=>'width:20px'),
-				),
-				array(
-						'header'=>'Fecha de término'.CHtml::image(Yii::app()->request->baseUrl."/imagenes/aplicacion/up-down.png", 'arriba-abajo', array('width'=>20)),
-						'name'=>'fecha_fin',
-						'filter'=>"<input type='date' name='Semana[fecha_fin]' min='".$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio)."' max='".$this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino)."' value='".$model->fecha_fin."'>",
-						'value'=>'SemanaController::fechaEvento($data->fecha_fin)',
-						'htmlOptions'=>array('style'=>'width:20px'),
-				),
-				array(
-						'header'=>'Institución'.CHtml::image(Yii::app()->request->baseUrl."/imagenes/aplicacion/up-down.png", 'arriba-abajo', array('width'=>20)),
-						'name'=>'institucion',
-						'filter'=>CHtml::textField('Semana[institucion]', $model->institucion, array('maxlength'=>255, 'size'=>10)),
-						'htmlOptions'=>array('style'=>'width:10px'),
-				),
-				array(
-						'type'=>'raw',
-						'value'=>'SemanaController::validaLogo($data)',
-						'header'=>'Logo',
-				),
+	'id' => 'semana-grid',
+	'dataProvider' => $model->search(),
+	'filter' => $model,
+	'columns' => array(
+		array(
+			'name' => 'estado_id',
+			'value' => 'Estado::model()->findByPk($data->estado_id)->nombre',
+			'filter' => CHtml::listData(Estado::model()->findAll(array('order' => 'nombre ASC')), 'id', 'nombre'),
+			'sortable' => false,
 		),
+		array(
+			'type' => 'raw',
+			'name' => 'sector',
+			'value' =>
+			'Semana::sector($data->sector)',
+			'filter' => Semana::sector(),
+			'sortable' => false,
+		),
+		array(
+			'header' => 'Institución' . CHtml::image(Yii::app()->request->baseUrl . "/imagenes/aplicacion/up-down.png", 'arriba-abajo', array('width' => 20)),
+			'name' => 'institucion',
+			'filter' => CHtml::textField('Semana[institucion]', $model->institucion, array('maxlength' => 255, 'size' => 10)),
+			'htmlOptions' => array('style' => 'width:10px'),
+		),
+		array(
+			'type' => 'raw',
+			'name' => 'publico_meta',
+			'value' =>
+			'Semana::publico_meta($data->publico_meta)',
+			'filter' => Semana::publico_meta(),
+			'sortable' => false,
+		),
+		array(
+			'type' => 'raw',
+			'name' => 'actividad',
+			'value' => '$data->actividad == "0" ? "<b>".$data->otra_actividad."</b><br>".(strlen($data->descripcion) <= 90 ? $data->descripcion: (substr($data->descripcion,0,90))." ...") : "<b>".Semana::actividades($data->actividad)."</b><br>".(strlen($data->descripcion) <= 90 ? $data->descripcion : (substr($data->descripcion,0,90))." ...")',
+			'filter' => Semana::actividades(),
+			//'htmlOptions'=>array('style'=>'width:40px'),
+			'sortable' => false,
+		),
+		array(
+			'type' => 'raw',
+			'name' => 'formato_actividad',
+			'value' =>
+			'Semana::formato_actividad($data->formato_actividad)',
+			'filter' => Semana::formato_actividad(),
+			'sortable' => false,
+		),
+		array(
+			'header' => 'Fecha de inicio' . CHtml::image(Yii::app()->request->baseUrl . "/imagenes/aplicacion/up-down.png", 'arriba-abajo', array('width' => 20)),
+			'name' => 'fecha_ini',
+			'filter' => "<input type='date' name='Semana[fecha_ini]' min='" . $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio) . "' max='" . $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino) . "' value='" . $model->fecha_ini . "'>",
+			'value' => 'SemanaController::fechaEvento($data->fecha_ini)',
+			'htmlOptions' => array('style' => 'width:20px'),
+		),
+		array(
+			'header' => 'Fecha de término' . CHtml::image(Yii::app()->request->baseUrl . "/imagenes/aplicacion/up-down.png", 'arriba-abajo', array('width' => 20)),
+			'name' => 'fecha_fin',
+			'filter' => "<input type='date' name='Semana[fecha_fin]' min='" . $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_inicio) . "' max='" . $this->formatoFecha('aaaa-mm-dd', Yii::app()->params->fecha_termino) . "' value='" . $model->fecha_fin . "'>",
+			'value' => 'SemanaController::fechaEvento($data->fecha_fin)',
+			'htmlOptions' => array('style' => 'width:20px'),
+		),
+		array(
+			'type' => 'raw',
+			'value' => 'SemanaController::validaLogo($data)',
+			'header' => 'Logo',
+		),
+	),
 )); ?>
