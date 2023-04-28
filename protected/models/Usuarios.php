6aa5c7c36ba13668e7a60de255ef8fbe27541293
Carlos Alonso <calonsogeek@gmail.com>
2023-04-28 10:17:41 -0600

    cambiando lineas del correo para mayo, mes

--- protected/models/Usuarios.php
+++ protected/models/Usuarios.php
@@ -157,13 +157,13 @@ Usuarios extends CActiveRecord
 	{
 		$imagen = "<table width=\"990\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\">";
 		$imagen .= "<tbody><tr><td width=\"790\" align=\"center\" bgcolor=\"#FFFFFF\">";
-		$imagen .= "<img src=\"https://www.biodiversidad.gob.mx/madretierra/imagenes/pagina/back_page-01.jpg\" width=\"707\">";
+		$imagen .= "<img src=\"https://www.biodiversidad.gob.mx/mesnaturaleza/imagenes/pagina/back_page-01.jpg\" width=\"707\">";
 		$imagen .= "</td></tr></tbody></table>";
 		$para = $this->email . ", sbd@conabio.gob.mx";
-		$titulo = "Dia Internacional de la Madre Tierra";
+		$titulo = "Mayo, mes de la naturaleza mexicana";
 		$mensaje = $imagen . "<br><br>" . $this->nombre . ' ' . $this->apellido . ",";
 		$mensaje .= "<br><br>Para poder poner una nueva contrase&ntilde;a sigue el siguiente ";
-		$mensaje .= "<a href=\"https://www.biodiversidad.gob.mx/madretierra/index.php?r=site/reset&id=" . $this->id . "&fec_alta=" . urlencode($this->fec_alta) . "\" target=\"_blank\">enlace</a>.";
+		$mensaje .= "<a href=\"https://www.biodiversidad.gob.mx/mesnaturaleza/index.php?r=site/reset&id=" . $this->id . "&fec_alta=" . urlencode($this->fec_alta) . "\" target=\"_blank\">enlace</a>.";
 		$cabeceras = "Content-type: text/html; charset=utf-8" . "\r\n";
 		$cabeceras .= "From: noreply@conabio.gob.mx" . "\r\n";
 		mail($para, $titulo, $mensaje, $cabeceras);
