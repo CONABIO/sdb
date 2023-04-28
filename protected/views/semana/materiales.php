184c7ed8779fe3a1f257450797bab52962906687
Laura Rojas <lrojas@conabio.gob.mx>
2023-04-28 08:21:14 -0600

    cambo de texto en materiales

--- protected/views/semana/materiales.php
+++ protected/views/semana/materiales.php
@@ -7,39 +7,48 @@
 	<!-- Linea --><hr/>
 	<ul>
 	  <li>
-	    <a href="https://www.un.org/es/observances/earth-day" target="_blank">Día internacional de la Madre Tierra</a>
+	    <a href="https://www.biodiversidad.gob.mx/diversidad/que-es" target="_blank">¿Qué es diversidad natural y cultural?</a>
 	  </li>
-	  <li><a href="https://cartadelatierra.org/lea-la-carta-de-la-tierra/preambulo/" target="_blank">La Carta de la Tierra</a><br>
+	  <li><a href="https://www.biodiversidad.gob.mx/diversidad/como-se-conserva" target="_blank">¿Cómo se conserva la diversidad biológica y cultural?</a><br>
       <a href="https://cartadelatierra.org/lea-la-carta-de-la-tierra/preambulo/"></a>      </li>
-	  <li><a href="https://www.tierramadre.org/que-es-tierra-madre/" target="_blank">Tierra Madre</a><br>
+	  <li><a href="https://www.biodiversidad.gob.mx/diversidad/alimentos/cocina-tradicionalmx" target="_blank">La cocina tradicional mexicana</a><br>
       </li>
-	  <li><a href="https://www.madretierramexico.org/" target="_blank">Madre Tierra México<br>
+	  <li><a href="https://www.biodiversidad.gob.mx/diversidad/artesanias" target="_blank">Artesanías<br>
       </a>      </li>
-	  <li><a href="https://www.gob.mx/semarnat/educacionambiental/es/videos/semarnat-en-coordinacion-con-la-sep-impulsan-la-educacion-ambiental-en-las-escuelas" target="_blank">SEP-SEMARNAT</a><br>
+	  <li><a href="https://www.biodiversidad.gob.mx/diversidad/fibras-naturales" target="_blank">Fibras naturales</a><br>
       </li>
+		<li><a href="https://www.biodiversidad.gob.mx/diversidad/ceremonial-y-ritual" target="_blank">Ceremonial y ritual</a><br>
+      </li>
+		<li><a href="https://www.biodiversidad.gob.mx/diversidad/medicinal" target="_blank">Medicina tradicional</a><br>
+      </li>
+		<li><a href="https://www.biodiversidad.gob.mx/diversidad/sistemas-productivos/turismo" target="_blank">Turismo sustentable</a><br>
+      </li>
+		<li><a href="https://www.biodiversidad.gob.mx/pais/cien-casos" target="_blank">Patrimonio natural de México, cien casos de éxito</a><br>
+      </li>
+	  <li><a href="https://www.biodiversidad.gob.mx/conabio/serie-billetes-g" target="_blank">Patrimonio biocultural en la nueva serie de billetes G</a><br></li>
+	<li><a href="https://www.biodiversidad.gob.mx/diversidad/patrimonio-biocultural" target="_blank">Patrimonio biocultural</a><br>
+      </li>
+	  <li><a href="https://www.biodiversidad.gob.mx/pais/patrimonio" target="_blank">Patrimonios de la humanidad en México</a></li>
+		<li><a href="https://www.biodiversidad.gob.mx/diversidad/alimentos/consumidor-responsable" target="_blank">Sé un consumidor responsable</a><br>
+	  </li>
   </ul>
   	
 	
 	<img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/ic_video.png" width="25" height="22" /> <span class="tit_materiales">Videos</span>
 	<!-- Linea --><hr/>	
 	<ul>
-      <li><a href="https://www.youtube.com/watch?v=wTkMFSgqi1I" target="_blank">La Naturaleza nos habla, Conservation International</a></li>
-      <li><a href="https://www.youtube.com/watch?v=kWTHU8kXF-c" target="_blank">La Tierra nos está hablando</a></li>
-      <li><a href="https://www.youtube.com/watch?v=dGQpIatGm28" target="_blank">EL secreto de nuestra madre Tonantzin </a></li>
+      <li><a href="https://youtu.be/imYZct6K1qI" target="_blank">Insectos de México. Insectos comestibles</a></li>
+      <li><a href="https://youtube.com/playlist?list=PLLAcoRHqCmNTg63DBGnqQyvOkDaTX01UC" target="_blank">Serie: Nos nutrimos de naturaleza</a></li>
+      <li><a href="https://youtu.be/--lqjLjguIs" target="_blank">CESMACH, una experiencia exitosa de sustentabilidad </a></li>
+	  <li><a href="https://youtu.be/0B2NRndO2pM" target="_blank">El Manglito, una experiencia exitosa de sustentabilidad </a></li>
+	  <li><a href="https://youtube.com/playlist?list=PLrFkZrRQk9nmzIwf9YNPdiXOcP6MW2oy_" target="_blank">De raíces mexicanas</a></li>
 	</ul>
 	
 	<img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/ic_audio.png" width="25" height="22" /> <span class="tit_materiales">Música</span>
 	<!-- Linea -->
 	<hr/>
     <ul>
-      <li><a href="https://www.youtube.com/watch?v=hb4Qc-Fyih4" target="_blank">El llamado</a></li>
-      <li><a href="https://www.youtube.com/watch?v=mGamAEDBP6M" target="_blank">Madre Tierra, Los cojolites<br>
-      </a></li>
-      <li><a href="https://www.youtube.com/watch?v=BrHb1AV8VS0" target="_blank">Huey Tonantzin, Abuela Malinalli</a><br>
-      </li>
-      <li><a href="https://www.youtube.com/watch?v=VkuRIZ7QyDM" target="_blank">Madre Tierra, Chayanne</a><br>
-      </li>
-      <li><a href="https://www.youtube.com/watch?v=BZGzBMFKUQQ" target="_blank">Canto a la Madre Tierra, Takiwaska</a><br>
+      <li><a href="https://www.youtube.com/watch?v=hb4Qc-Fyih4" target="_blank">El llamado</a><br>
       </li>
     </ul><br>
 </div>
