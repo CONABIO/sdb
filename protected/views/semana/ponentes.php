d5362bbcf954c596bb6c42beb1b29a182ba24d78
Laura Rojas <lrojas@conabio.gob.mx>
2023-05-18 18:20:31 -0600

    actualización página ponentes e imágenes

--- protected/views/semana/ponentes.php
+++ protected/views/semana/ponentes.php
@@ -18,6 +18,22 @@ $( "#accordion" ).accordion({
 
 <h1>Charlas</h1><br>
 
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/ic_foto.png" width="25" height="22" /> <span class="tit_materiales">Invitación y programa</span>
+	<!-- Linea -->
+	<hr/>	
+	<p><a href="https://www.biodiversidad.gob.mx/mesnaturaleza/imagenes/pagina/MMNat_invitacion_MNA.jpg" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/imagenes/pagina/MMNat_invita22_MNA_peq.jpg" width="300" height="191"></a>
+	  <br>
+</p>
+	<p><br>
+	  
+</p>
+  <h4>Presentación App Enciclovida 2.0</h4>
+<!-- Linea -->
+<hr>
+
+<iframe width="50%" height="235" src="https://www.youtube.com/embed/T5rhfWyxFR8" frameborder="0" allowfullscreen=""></iframe>	
+  <p>Conoce las nuevas funciones de esta increíble aplicación que incluye información de la flora y la fauna de México.</p>
+  <br>
 
 
 <div style="width: 100%; display: ; text-align: center; float:left; display:;">
